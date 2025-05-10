cbdc24fedb6e65de6b3eb0fec309b8a528d542d0
njm-solus <njm.solus@gmail.com>
2025-05-10 19:59:02 +0300

    [Ticket: X]

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,22 @@
 # pcost.py
 #
 # Exercise 1.27
+def portfolio_cost(filename):
+
+
+    cost_a = 0
+    with open(filename, 'rt') as f:
+        try:
+            headers = next(f).split(',')
+        except ValueError:
+            print(headers)
+
+        for line in f:
+            row = line.split(',')
+            s_cost = int(row[1]) * float(row[2])
+            cost_a += s_cost
+        return cost_a
+
+
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total cost: ',cost)
